1b7188031688480e8d58acf0e6129db10c786097
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-09-18 10:06:05 -0600

    completes iteration 2

--- lib/curator.rb
+++ lib/curator.rb
@@ -14,4 +14,16 @@ class Curator
   def add_artist(artist)
     @artists << Artist.new(artist)
   end
+
+  def find_artist_by_id(id)
+    @artists.find do |artist|
+      artist.id == id
+    end
+  end
+
+  def find_photograph_by_id(id)
+    @photographs.find do |photograph|
+      photograph.id == id
+    end
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -89,6 +89,61 @@ class CuratorTest < Minitest::Test
     assert artist_1, curator.artists.first
 
     assert "Henri Cartier-Bresson",curator.artists.first.name
+  end
 
+  def test_curator_can_find_artist_by_id
+    curator = Curator.new
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"        }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+      }
+
+    curator.add_artist(artist_1)
+
+    curator.add_artist(artist_2)
+
+    assert_instance_of Artist,
+    curator.find_artist_by_id("1")
+
+    assert artist_1, curator.find_artist_by_id("1")
+
+    assert_equal "Henri Cartier-Bresson", curator.find_artist_by_id("1").name
+  end
+
+  def test_curator_can_find_photograph_by_id
+    curator = Curator.new
+
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+
+    assert_instance_of Photograph, curator.find_photograph_by_id("2")
+
+    assert photo_2, curator.find_photograph_by_id("2")
+
+    assert_equal "1941", curator.find_photograph_by_id("2").year
   end
 end
