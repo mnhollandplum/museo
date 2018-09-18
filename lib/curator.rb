bab3bb3e2ef0afe530449d113194b5b8b67495b9
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-09-18 11:12:44 -0600

    finishes iteration 3, needs refactoring

--- lib/curator.rb
+++ lib/curator.rb
@@ -26,4 +26,46 @@ class Curator
       photograph.id == id
     end
   end
+
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photograph|
+      photograph.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    ids =   photos_by_artist_id.map do |id, photos|
+        if photos.count > 1
+          id
+        end
+      end.compact
+    @artists.find_all do |artist|
+      [artist.id] == ids
+    end
+  end
+
+  def photos_by_artist_id
+    @photographs.group_by do |photograph|
+      photograph.artist_id
+    end
+  end
+
+  def photographs_taken_by_artists_from(country)
+    artists_id_by_country = @artists.map do |artist|
+      if artist.country == country
+        artist.id
+      end
+    end.compact
+    artists_id_by_country.map do |id|
+        find_photograph_by_artist_id(id)
+    end.flatten
+  end
+
+  def find_photograph_by_artist_id(id)
+    @photographs.find_all do |photo|
+      photo.artist_id == id
+    end
+  end
+
+
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/curator'
+require 'pry'
 
 class CuratorTest < Minitest::Test
   def test_curator_exists
@@ -145,5 +146,217 @@ class CuratorTest < Minitest::Test
     assert photo_2, curator.find_photograph_by_id("2")
 
     assert_equal "1941", curator.find_photograph_by_id("2").year
+  end
+
+  def test_curator_can_find_photographs_by_artist
+    curator = Curator.new
+
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+
+    diane_arbus = curator.find_artist_by_id("3")
+
+    assert [photo_3, photo_4], curator.find_photographs_by_artist(diane_arbus)
+
+    assert_equal "Identical Twins, Roselle, New Jersey", curator.find_photographs_by_artist(diane_arbus).first.name
+  end
+
+  def test_curator_can_find_artists_with_multiple_photographs
+    curator = Curator.new
+
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+
+    diane_arbus = curator.find_artist_by_id("3")
+
+    assert_equal "1", curator.photos_by_artist_id.first[0]
+
+    assert_instance_of Artist, curator.artists_with_multiple_photographs.first
+
+    assert_equal "Diane Arbus", curator.artists_with_multiple_photographs.first.name
+
+    assert_equal 1, curator.artists_with_multiple_photographs.length
+
+    assert_equal diane_arbus, curator.artists_with_multiple_photographs.first
+  end
+
+  def test_curator_can_find_photographs_taken_by_artists_from_a_country
+    curator = Curator.new
+
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+
+    assert_equal "Moonrise, Hernandez", curator.photographs_taken_by_artists_from("United States").first.name
+
+    assert_equal "Child with Toy Hand Grenade in Central Park", curator.photographs_taken_by_artists_from("United States").last.name
+
+    assert_equal [], curator.photographs_taken_by_artists_from("Argentina")
+
+
+
+
+
   end
 end
